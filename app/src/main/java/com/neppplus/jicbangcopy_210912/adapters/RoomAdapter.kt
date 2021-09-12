9819187603b7913aeb5a5b591c1d5bcb32939b22
이진호 <spris1513@gmail.com>
2021-09-12 12:39:11 +0900

    RoomAdapter 작업 마무리

--- app/src/main/java/com/neppplus/jicbangcopy_210912/adapters/RoomAdapter.kt
+++ app/src/main/java/com/neppplus/jicbangcopy_210912/adapters/RoomAdapter.kt
@@ -1,7 +1,11 @@
 package com.neppplus.jicbangcopy_210912.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.jicbangcopy_210912.R
 import com.neppplus.jicbangcopy_210912.datas.RoomData
 
 class RoomAdapter(
@@ -10,5 +14,16 @@ class RoomAdapter(
     val mlist: ArrayList<RoomData>) : ArrayAdapter<RoomData>(mContext,resId,mlist) {
 
 
+    val mInflater = LayoutInflater.from(mContext)
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow==null){
+            tempRow = mInflater.inflate(R.layout.roon_list_item,null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
 
 }
