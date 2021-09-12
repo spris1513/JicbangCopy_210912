e0f5f2ffad67309ea94fae9814bbcc67bc353cb2
이진호 <spris1513@gmail.com>
2021-09-12 14:57:16 +0900

    받아낸 방 데이터 활용 > 실제문구반영  - 클레스에 만들어둔 함수 재활용

--- app/src/main/java/com/neppplus/jicbangcopy_210912/activity_view_room_detail.kt
+++ app/src/main/java/com/neppplus/jicbangcopy_210912/activity_view_room_detail.kt
@@ -3,6 +3,7 @@ package com.neppplus.jicbangcopy_210912
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.neppplus.jicbangcopy_210912.datas.RoomData
+import kotlinx.android.synthetic.main.activity_view_room_detail.*
 
 class activity_view_room_detail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +11,11 @@ class activity_view_room_detail : AppCompatActivity() {
         setContentView(R.layout.activity_view_room_detail)
 
         val room = intent.getSerializableExtra("roomData") as RoomData
+
+        priceTxt.text = room.gerFormattedPrice()
+        descriptionTxt.text = room.description
+        adressTxt.text = room.address
+        floorTxt.text = room.getFormatedFloor()
 
 
     }
